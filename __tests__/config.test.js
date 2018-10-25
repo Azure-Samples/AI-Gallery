641d6e8654a6bb10ecf8555998e8c263ded04684
Jeremy Kramer <jekrame@microsoft.com>
2018-10-25 11:16:55 -0700

    remove browse again

--- __tests__/config.test.js
+++ __tests__/config.test.js
@@ -1,9 +1,5 @@
 const config = require('./../src/config');
 
-test('confirm getData returns data', () =>{
-    expect(config.getData()).toBeDefined();
-})
-
 test('confirm getData has models', () =>{
     expect(config.getData()).toContain('models');
 })
