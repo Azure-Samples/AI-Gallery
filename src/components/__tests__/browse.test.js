3d857d41cbe670e55eaabe3bc7afbeb7ac8e1378
Azure AI Gallery Team <aigallery@microsoft.com>
2018-12-10 19:14:36 +0000

    VSTS CI Commit: Build 2002329

--- src/components/__tests__/browse.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer'
-import Browse from './../browse';
-
-
-test('renders a snapshot of browse', () => {
-    const tree = renderer.create(<Browse/>).toJSON();
-    expect(tree).toMatchSnapshot();
-})
